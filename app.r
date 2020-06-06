7e227f7f59079677a04d7cfce70fb147b1e02b09
João Pedro Chagas <jpchagas@gmail.com>
2020-06-05 21:43:20 -0300

    Begin project

---
+++ app.r
@@ -0,0 +1,6 @@
+install.packages('twitteR')
+install.packages('plyr')
+install.packages('stringr')
+install.packages('tm')
+install.packages('sentimentr')
+install.packages('Rstem',repos = "http://www.omegahat.net/R")
