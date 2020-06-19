cef4f46ac21a808e9273bfe8b2c9adaa59379106
dspaulolima <ds.paulolima@gmail.com>
2020-06-19 19:06:07 -0300

    functions - extract, five_most, @users - updated

--- Python/functions.py
+++ Python/functions.py
@@ -1,14 +1,28 @@
 import tw_auth as tw
+
 import pandas as import pd
+import numpy as np
+
+import plotly
+import matplotlib.pyplot as plt
+
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk import FreqDist
+
+from wordcloud import WordCloud
+from string import punctuation
+
 
 # FUNCTION OF 5 MOST RECENT TWEETS WITH THE HIGHEST NUMBER OF RETWEETS
 
-query = 'felicidade'
-q_filter = ' -filter:retweet'
+query = 'covid'
 
-def extract_data(query=query, head=5, lang='pt', items=200):
+def extract_data(query, head=5, lang='pt', items=500):
     
-    TWITTER_DATA = []
+    RETWEET = []
+    TWEET = []
 
     for tweet in tw.tweepy.Cursor(tw.api.search,
                                   q=query,
@@ -17,17 +31,23 @@ def extract_data(query=query, head=5, lang='pt', items=200):
                                   tweet_mode='extended'  # collect the full text (over 140 characters)
                                   ).items(items):
 
-        TWITTER_DATA.append([tweet.id, tweet.created_at, tweet.user.location, tweet.full_text.replace('\n', ' '), 
+        if ("RT @" in tweet.full_text):
+
+            RETWEET.append([tweet.id, tweet.created_at, tweet.user.location, tweet.full_text.replace('\n', ' '), 
                                           tweet.retweet_count, [e['text'] for e in tweet._json['entities']['hashtags']]])
 
-        df = pd.DataFrame(TWITTER_DATA, columns=['id','timestamp', 'location', 'tweet', 'retweet_count', 'hashtag'])
+        else:
+
+            TWEET.append([tweet.id, tweet.created_at, tweet.user.location, tweet.full_text.replace('\n', ' '), 
+                                          tweet.retweet_count, [e['text'] for e in tweet._json['entities']['hashtags']]])
         
-    return df
+    return TWEET, RETWEET
 
 
-def five_most_recent_highest_retweets(data = df, head=5):
+def five_most_recent_highest_retweets(data = rtw, head=5):
     
-    df_five = data[['tweet', 'retweet_count']].drop_duplicates() # delete all duplicated texts
+    rtweets = pd.DataFrame(data, columns=['id', 'timestamp', 'location', 'tweet', 'retweet_count', 'hashtags'])
+    df_five = rtweets[['tweet', 'retweet_count']].drop_duplicates() # delete all duplicated texts
     df_five = df_five.groupby('tweet').sum() # aggregate the same texts and adds up the number of different retweets.
     df_five = df_five.sort_values(by='retweet_count', ascending = False) # put retweets_count in descending order
 
@@ -51,9 +71,11 @@ def most_hashtag():
 
 # FUNCTION OF MOST CITED @USER ACCOUNTS IN THE TWEETS
 
-def most_arroba(data = df, query=''.join([query, q_filter])):
+def most_arroba(data = tw):
 
-    tweets = df['tweet']
+    tw = pd.DataFrame(data, columns=['id', 'timestamp', 'location', 'tweet', 'retweet_count', 'hashtags'])
+
+    tweets = tw['tweet']
     arroba = []
 
     for line in tweets:
@@ -62,7 +84,9 @@ def most_arroba(data = df, query=''.join([query, q_filter])):
             if word.startswith("@"):
                 arroba.append(word)
 
-    return pd.DataFrame(users)[0].value_counts()
+    top_10 = pd.DataFrame(arroba)[0].value_counts().sort_values(ascending=False).head(10)
+
+    return top_10
 
 # FUNCTION OF MOST USED WORDS IN TWEETS DISREGARDING STOPWORDS
 
